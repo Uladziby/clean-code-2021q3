836ba35e4b270864066ca4835b2143ec2eb5621b
Vlad Yanushevsky <yanushevskyv@gmail.com>
2021-11-29 03:01:01 +0300

    feat :  create branch for deploy

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ addButton.addEventListener("click", addTask);
 addButton.addEventListener("click", ajaxRequest);
 
 var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
-  console.log("bind list item events", taskListItem);
+  console.log("bind list item events");
   //select ListItems children
   var checkBox = taskListItem.querySelector(".list_checkbox");
   console.log(checkBox);
